319cdf52d043a6be684d83614c16644df0bbabf2
Anuj <Anuj-kumar.sen@postnord.com>
2021-10-31 19:32:16 +0100

    init

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Configure the Azure provider
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rgname
+  location = var.location
+}
