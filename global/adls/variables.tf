a83e09efc2f791ac1c6db269f103ad4d191bee24
Anuj <Anuj-kumar.sen@postnord.com>
2021-12-06 10:42:55 +0100

    remove comment

--- global/adls/variables.tf
+++ global/adls/variables.tf
@@ -1,22 +1,3 @@
-/*variable "SUBSCRIPTION_ID" {
-  type        = string
-  description = "SUBSCRIPTION_ID for Azure Service principal ."
-}
-
-variable "TENANT_ID" {
-  type        = string
-  description = "SUBSCRIPTION_ID for Azure Service principal ."
-}
-
-variable "CLIENT_ID" {
-  type        = string
-  description = "SUBSCRIPTION_ID for Azure Service principal ."
-}
-variable "CLIENT_SECRET" {
-  type        = string
-  description = "SUBSCRIPTION_ID for Azure Service principal ."
-
-}*/
 
 variable "rgname" {
   type        = string
